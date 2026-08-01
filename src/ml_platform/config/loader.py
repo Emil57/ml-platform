e306babb89eadc4b2ef9c1b8464bcc14d7e4d401
Emil57 <trejocovantesde@gmail.com>
2026-08-01 16:54:40 -0700

    Refactor code

--- src/ml_platform/config/loader.py
+++ src/ml_platform/config/loader.py
@@ -1,3 +1,9 @@
 from .settings import Settings
 
-settings = Settings()
+
+def load_settings() -> Settings:
+    """Create and return the platform settings."""
+    return Settings()
+
+
+settings = load_settings()
