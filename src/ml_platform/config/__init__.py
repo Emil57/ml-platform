789b9445b445879d89b95405c49eb1bbd6441175
Emil57 <trejocovantesde@gmail.com>
2026-08-01 16:46:22 -0700

    🔒️#8: Set up global paths file for config

---
+++ src/ml_platform/config/__init__.py
@@ -0,0 +1,29 @@
+"""
+Configuration package.
+"""
+
+from .paths import (
+    DATA_DIR,
+    DOCS_DIR,
+    EXTERNAL_DATA_DIR,
+    LOGS_DIR,
+    MODELS_DIR,
+    NOTEBOOKS_DIR,
+    PROCESSED_DATA_DIR,
+    RAW_DATA_DIR,
+    ROOT_DIR,
+    TESTS_DIR,
+)
+
+__all__ = [
+    "ROOT_DIR",
+    "DATA_DIR",
+    "RAW_DATA_DIR",
+    "PROCESSED_DATA_DIR",
+    "EXTERNAL_DATA_DIR",
+    "MODELS_DIR",
+    "DOCS_DIR",
+    "TESTS_DIR",
+    "LOGS_DIR",
+    "NOTEBOOKS_DIR",
+]
